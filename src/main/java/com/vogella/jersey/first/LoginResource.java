c7cc5884855aa30e557175527a5f2604ee14c01a
marti <martijn_bakker@upcmail.nl>
2018-01-23 12:33:20 +0100

    added removed classes

--- src/main/java/com/vogella/jersey/first/LoginResource.java
+++ src/main/java/com/vogella/jersey/first/LoginResource.java
@@ -1,5 +1,9 @@
 package com.vogella.jersey.first;
 
+import com.vogella.jersey.first.Model.ClassController;
+import com.vogella.jersey.first.Model.Database;
+import com.vogella.jersey.first.repDatabase.RepConnector;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
@@ -14,6 +18,16 @@ public class LoginResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String Login() {
+        ClassController cC = new ClassController();
+
+        StringBuilder tables = new StringBuilder();
+        //RepConnector c = new RepConnector(url, port, service, username, password);
+        RepConnector c = new RepConnector("ondora02.hu.nl", "8521","cursus02.hu.nl", "tosad_2017_2b_team5_target", "tosad_2017_2b_team5_target");
+        Database dat = new Database("target");
+
+        cC.loadDatabase("ondora", c.GetDatabase());
+
+
 
         return "succes";
     }
