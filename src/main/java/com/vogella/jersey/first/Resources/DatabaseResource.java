261b35063fb14997dd4e8d5fc2c5a899f2624e0f
marti <martijn_bakker@upcmail.nl>
2018-01-23 22:52:05 +0100

    - added test for json

--- src/main/java/com/vogella/jersey/first/Resources/DatabaseResource.java
+++ src/main/java/com/vogella/jersey/first/Resources/DatabaseResource.java
@@ -3,6 +3,10 @@ package com.vogella.jersey.first.Resources;
 import com.vogella.jersey.first.Model.ClassController;
 import com.vogella.jersey.first.DOA.TargetConnector;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
@@ -14,6 +18,7 @@ import javax.ws.rs.core.MediaType;
 public class DatabaseResource{
 
     @GET
+    @Path("/init")
     @Produces(MediaType.APPLICATION_JSON)
     public String Login(@QueryParam("url")  String url,@QueryParam("port")  String port,@QueryParam("service")  String service,@QueryParam("username")  String username,@QueryParam("password")  String password) {
         try {
@@ -28,5 +33,21 @@ public class DatabaseResource{
             return "wrong input data";
         }
 
+    }
+
+    @GET
+    @Path("/tables")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getTables(){
+
+        JsonArray value = Json.createArrayBuilder()
+                .add(Json.createObjectBuilder()
+                        .add("type", "home")
+                        .add("number", "212 555-1234"))
+                .add(Json.createObjectBuilder()
+                        .add("type", "fax")
+                        .add("number", "646 555-4567"))
+                .build();
+        return value.toString();
     }
 }
