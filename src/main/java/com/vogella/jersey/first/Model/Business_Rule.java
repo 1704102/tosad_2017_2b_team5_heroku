a9db7a5f4582b3a32ddc5fccbbba00721e0203bd
Sijmen <Sijmenj@gmail.com>
2018-01-24 12:00:18 +0100

    Commited a change to business_Rules, can now reconize 3 of the 4 business rule types and added validation from the business range rule to make sure value errors don't occur.

--- src/main/java/com/vogella/jersey/first/Model/Business_Rule.java
+++ src/main/java/com/vogella/jersey/first/Model/Business_Rule.java
@@ -1,28 +1,45 @@
 package com.vogella.jersey.first.Model;
 
 public class Business_Rule {
-    private Operator operator;
+    String brName;
+    private String operator;
     private int value1, value2;
     private Table table1, table2;
     private Column column1, column2;
     private String type;
-
+    //This is the constructor for range rules
     public Business_Rule(int value1, int value2,Table table1,Column column1){
         this.value1=value1;
         this.value2=value2;
         this.table1=table1;
         this.column1=column1;
         type="rangeRule";
+        validateRangeRule();
     }
-    public Business_Rule(Table table1, Table table2, Column column1, Column column2, String operator){
+    //This is the constructor for tuple rules
+    public Business_Rule(Table table1, Column column1, Column column2, String operator){
         this.table1 = table1;
-        this.table2 = table2;
         this.column1 = column1;
         this.column2 = column2;
         Operator o = new Operator(operator);
-        this.operator = o;
-
-
-
+        this.operator = operator;
+        type= "tupleRule";
+    }
+    //This is the constructor for attribute rules
+    public Business_Rule(Table table1, Column column1,String operator){
+        this.table1=table1;
+        this.column1=column1;
+        this.operator=operator;
+        type="attributerule";
+    }
+    //A validationCheck in place for range rule to make sure value1 has a higher value then value2.
+// This prevents problems when creating the final business rules because they will not work when the values are in the opposite order
+    private void validateRangeRule(){
+        if (value1>value2){
+            int rotatevalue;
+            rotatevalue= value1;
+            value1=value2;
+            value2=rotatevalue;
+        }
     }
 }
